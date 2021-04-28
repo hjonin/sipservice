e77f3999580bd63f4a88b0b14e388d4a2cc748d3
Hélène Jonin <helene.jonin@culture.gouv.fr>
2021-04-28 10:10:17 +0200

    Add CORS configuration for SEDAccord Angular application

--- src/main/java/fr/gouv/vitam/sipservice/SipserviceApplication.java
+++ src/main/java/fr/gouv/vitam/sipservice/SipserviceApplication.java
@@ -2,6 +2,10 @@ package fr.gouv.vitam.sipservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SipserviceApplication {
@@ -10,4 +14,16 @@ public class SipserviceApplication {
 		SpringApplication.run(SipserviceApplication.class, args);
 	}
 
+	@Profile("dev")
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/sip/*")
+						.allowedOrigins("http://localhost:4200")
+						.exposedHeaders("Content-Disposition");
+			}
+		};
+	}
 }
